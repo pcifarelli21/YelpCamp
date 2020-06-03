03b6903d36ef9e77c487c0dffdb48e1c081d712c
Peter C <55725019+pcifarelli21@users.noreply.github.com>
2020-06-03 17:27:53 -0500

    create app with preset arrays

---
+++ v1_Array/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+let campgrounds = [
+    {name:"Samon Creek", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name:"Granite Hill", image: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name:"Mountain Goats Rest", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name:"Samon Creek", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name:"Granite Hill", image: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name:"Mountain Goats Rest", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name:"Samon Creek", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name:"Granite Hill", image: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name:"Mountain Goats Rest", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
+]
+
+app.get('/', (req, res) => {
+    res.render("landing.ejs")
+})
+
+app.get("/campgrounds", (req, res) => {
+    res.render("campgrounds.ejs", {campgrounds:campgrounds});
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render("new.ejs")
+})
+
+app.post('/campgrounds', function(req, res){
+    //get data from form and add to campground array
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = {name: name, image:image}
+    campgrounds.push(newCampground);
+
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds");
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Server started on port: ${port}`)
+})
+
